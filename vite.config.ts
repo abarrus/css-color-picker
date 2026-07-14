99a501d892579819a082f5c0b476bd72f80906e1
abarrus <67875711+abarrus@users.noreply.github.com>
2026-07-14 09:38:16 -0400

    attempt to make it work on github pages?

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+
+const basePath = (process.argv.includes('dev') ? '' : process.env.BASE_PATH) as `/${string}` | '';
 
 export default defineConfig({
 	plugins: [
@@ -14,7 +16,16 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: undefined,
+				precompress: false,
+				strict: true
+			}),
+			paths: {
+				base: basePath
+			}
 		})
 	]
 });
